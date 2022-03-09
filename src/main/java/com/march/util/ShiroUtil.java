ab60646f6545b566675bb75e6718a3e88efa0d73
Aldebaranor <2659460681@qq.com>
2022-03-09 20:56:30 +0800

    shiro

---
+++ src/main/java/com/march/util/ShiroUtil.java
@@ -0,0 +1,12 @@
+package com.march.util;
+
+import com.march.shiro.AccountProfile;
+import org.apache.shiro.SecurityUtils;
+
+public class ShiroUtil {
+
+    public static AccountProfile getProfile() {
+        return (AccountProfile) SecurityUtils.getSubject().getPrincipal();
+    }
+
+}
