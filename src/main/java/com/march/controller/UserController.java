638c92706cc39783c12bf34ff3fc033a7d128157
Aldebaranor <2659460681@qq.com>
2022-03-13 17:25:29 +0800

    march 13th

--- src/main/java/com/march/controller/UserController.java
+++ src/main/java/com/march/controller/UserController.java
@@ -4,11 +4,10 @@ package com.march.controller;
 import com.march.common.lang.Result;
 import com.march.entity.User;
 import com.march.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -25,10 +24,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @RequiresAuthentication
     @GetMapping("/index")
     public Object index(){
         User user = userService.getById(1);
         return Result.succ(200,"操作成功",user);
+    }
+
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user){
+        return Result.succ(user);
     }
 
 }
